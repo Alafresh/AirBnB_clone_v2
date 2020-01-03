9d07705981c5d27dee21942953ebe86c792dd81e
Alafresh <868@holbertonschool.com>
2020-01-03 21:54:16 +0000

    Compress before sending

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -3,10 +3,13 @@
 from datetime import datetime
 from fabric.api import local
 
+
 def do_pack():
     local('mkdir -p versions')
     time = datetime.now()
     folder = 'web_static'
-    name_file = "{}{}{}{}{}{}{}".format(folder, time.year, time.month, time.day, time.hour, time.minute, time.second)
+    name_file = "{}{}{}{}{}{}{}".format(folder,
+                                        time.year, time.month, time.day,
+                                        time.hour, time.minute, time.second)
     local('tar -cavf {}.tar.gz web_static'.format(name_file))
     local('mv *.tar.gz versions/')
