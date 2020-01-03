43144c01b42419f5113d7a675ee1b251f73bfb2d
Alafresh <868@holbertonschool.com>
2020-01-03 23:17:45 +0000

    again

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 # Fabric script that generates a .tgz
 from datetime import datetime
-from fabric.api import local, put, run
+from fabric.api import *
 from os import path
 
 env.hosts = ['35.231.113.169', '34.73.247.107']
@@ -11,27 +11,22 @@ def do_pack():
     local('mkdir -p versions')
     time = datetime.now()
     folder = 'web_static_'
-    name_file = "{}{}{}{}{}{}{}.tgz".format(folder, time.year,
-                                            time.month, time.day,
-                                            time.hour, time.minute,
-                                            time.second)
+    name_file = "{}{}{}{}{}{}{}.tgz".format(folder,
+                                        time.year, time.month, time.day,
+                                        time.hour, time.minute, time.second)
     local('tar -cavf {} web_static'.format(name_file))
     local('mv *.tgz versions/')
 
-
 def do_deploy(archive_path):
-    if not path.exists('archive_path'):
+    if path.exists('archive_path') == False:
         return False
     aux = [x for x in archive_path.split('.') if x.strip()]
     tmp = aux[0]
     put('archive_path', "/tmp/")
     run('mkdir -p /data/web_static/releases/{}'.format(tmp))
-    run('tar -xzf /tmp/{} -C \
-     /data/web_static/releases/{}'.format(archive_path, tmp))
+    run('tar -xzf /tmp/{} -C /data/web_static/releases/{}'.format(archive_path, tmp))
     run('rm /tmp/{}'.format(archive_path))
-    run('mv /data/web_static/releases/{}/web_static/* \
-     /data/web_static/releases/{}/'.format(tmp, tmp))
+    run('mv /data/web_static/releases/{}/web_static/* /data/web_static/releases/{}/'.format(tmp, tmp))
     run("rm -rf /data/web_static/releases/{}/web_static".format(tmp))
     run('rm -rf /data/web_static/current')
-    run('ln -s /data/web_static/releases/{}/ \
-    /data/web_static/current'.format(tmp))
+    run('ln -s /data/web_static/releases/{}/ /data/web_static/current'.format(tmp))
