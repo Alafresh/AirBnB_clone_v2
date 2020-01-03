6ce3bf2f6bff3b48a0a3147d8d9bad3688c04282
Alafresh <868@holbertonschool.com>
2020-01-03 22:07:04 +0000

    aaah porfin

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -8,8 +8,8 @@ def do_pack():
     local('mkdir -p versions')
     time = datetime.now()
     folder = 'web_static_'
-    name_file = "{}{}{}{}{}{}{}".format(folder,
+    name_file = "{}{}{}{}{}{}{}.tgz".format(folder,
                                         time.year, time.month, time.day,
                                         time.hour, time.minute, time.second)
-    local('tar -cavf {}.tar.gz web_static'.format(name_file))
-    local('mv *.tar.gz versions/')
+    local('tar -cavf {} web_static'.format(name_file))
+    local('mv *.tgz versions/')
