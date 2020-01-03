6fb28b64e85bbdb652736b45828f9c1b6e4045af
Alafresh <868@holbertonschool.com>
2020-01-03 18:24:25 +0000

    first commit

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+# This script prepare the web server to deploy static content
+mkdir -p /data/web_static/releases/test/
+mkdir -p /data/web_static/shared/
+touch /data/web_static/releases/test/index.html
+echo "<html><head><title>Test</title></head><body><h1>Test</h1></body></html>" > /data/web_static/releases/test/index.html
+ln -snf data/web_static/releases/test/ /data/web_static/current
+chown -R ubuntu:ubuntu data/
+sed -i "49i location /hbnb_static/ {" /etc/nginx/sites-available/default
+sed -i "50i alias /data/web_static/current/;" /etc/nginx/sites-available/default
+sed -i "51i autoindex off;" /etc/nginx/sites-available/default
+sed -i "52i }" /etc/nginx/sites-available/default
+service nginx restart
