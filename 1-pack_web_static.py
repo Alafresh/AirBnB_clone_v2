191758f12498f60b6cf25aedec950a2d64e9120e
Alafresh <868@holbertonschool.com>
2020-01-03 22:08:47 +0000

    Compress before sendinggit add 1-pack_web_static.py

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -8,8 +8,9 @@ def do_pack():
     local('mkdir -p versions')
     time = datetime.now()
     folder = 'web_static_'
-    name_file = "{}{}{}{}{}{}{}.tgz".format(folder,
-                                        time.year, time.month, time.day,
-                                        time.hour, time.minute, time.second)
+    name_file = "{}{}{}{}{}{}{}.tgz".format(folder, time.year,
+                                            time.month, time.day,
+                                            time.hour, time.minute,
+                                            time.second)
     local('tar -cavf {} web_static'.format(name_file))
     local('mv *.tgz versions/')
