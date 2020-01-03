80a748b196cac5b3546b6c08f06f33ff5717527c
Alafresh <868@holbertonschool.com>
2020-01-03 21:52:00 +0000

    i Hate fabric

---
+++ 1-pack_web_static.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# Fabric script that generates a .tgz
+from datetime import datetime
+from fabric.api import local
+
+def do_pack():
+    local('mkdir -p versions')
+    time = datetime.now()
+    folder = 'web_static'
+    name_file = "{}{}{}{}{}{}{}".format(folder, time.year, time.month, time.day, time.hour, time.minute, time.second)
+    local('tar -cavf {}.tar.gz web_static'.format(name_file))
+    local('mv *.tar.gz versions/')
