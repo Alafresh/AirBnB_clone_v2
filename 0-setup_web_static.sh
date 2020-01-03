2c44777f393edf5f206772a5df32a464c79cad1e
Alafresh <868@holbertonschool.com>
2020-01-03 18:31:46 +0000

    add / path

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -4,8 +4,8 @@ mkdir -p /data/web_static/releases/test/
 mkdir -p /data/web_static/shared/
 touch /data/web_static/releases/test/index.html
 echo "<html><head><title>Test</title></head><body><h1>Test</h1></body></html>" > /data/web_static/releases/test/index.html
-ln -snf data/web_static/releases/test/ /data/web_static/current
-chown -R ubuntu:ubuntu data/
+ln -snf /data/web_static/releases/test/ /data/web_static/current
+chown -R ubuntu:ubuntu /data/
 sed -i "49i location /hbnb_static/ {" /etc/nginx/sites-available/default
 sed -i "50i alias /data/web_static/current/;" /etc/nginx/sites-available/default
 sed -i "51i autoindex off;" /etc/nginx/sites-available/default
