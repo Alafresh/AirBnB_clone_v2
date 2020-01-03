63a853ece9d364f09bdff25fdd48a92ea4fe8725
Alafresh <868@holbertonschool.com>
2020-01-03 23:33:14 +0000

    hey

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -11,9 +11,10 @@ def do_pack():
     local('mkdir -p versions')
     time = datetime.now()
     folder = 'web_static_'
-    name_file = "{}{}{}{}{}{}{}.tgz".format(folder,
-                                        time.year, time.month, time.day,
-                                        time.hour, time.minute, time.second)
+    name_file = "{}{}{}{}{}{}{}.tgz".format(folder, time.year,
+                                            time.month, time.day,
+                                            time.hour, time.minute,
+                                            time.second)
     local('tar -cavf {} web_static'.format(name_file))
     local('mv *.tgz versions/')
 
@@ -22,11 +23,17 @@ def do_deploy(archive_path):
         return False
     aux = [x for x in archive_path.split('.') if x.strip()]
     tmp = aux[0]
-    put('archive_path', "/tmp/")
-    run('mkdir -p /data/web_static/releases/{}'.format(tmp))
-    run('tar -xzf /tmp/{} -C /data/web_static/releases/{}'.format(archive_path, tmp))
-    run('rm /tmp/{}'.format(archive_path))
-    run('mv /data/web_static/releases/{}/web_static/* /data/web_static/releases/{}/'.format(tmp, tmp))
-    run("rm -rf /data/web_static/releases/{}/web_static".format(tmp))
-    run('rm -rf /data/web_static/current')
-    run('ln -s /data/web_static/releases/{}/ /data/web_static/current'.format(tmp))
+    try:
+        put('archive_path', "/tmp/")
+        run('mkdir -p /data/web_static/releases/{}'.format(tmp))
+        run('tar -xzf /tmp/{} -C \
+        /data/web_static/releases/{}'.format(archive_path, tmp))
+        run('rm /tmp/{}'.format(archive_path))
+        run('mv /data/web_static/releases/{}/web_static/* \
+        /data/web_static/releases/{}/'.format(tmp, tmp))
+        run("rm -rf /data/web_static/releases/{}/web_static".format(tmp))
+        run('rm -rf /data/web_static/current')
+        run('ln -s /data/web_static/releases/{}/ /data/web_static/current'.format(tmp))
+        return True
+    except:
+        return False
