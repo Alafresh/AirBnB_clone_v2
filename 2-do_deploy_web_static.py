405707aec3ee9b8f4b658281958b8334b5de0496
Alafresh <868@holbertonschool.com>
2020-01-04 00:18:40 +0000

    please

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 # Fabric script that generates a .tgz
 from datetime import datetime
-from fabric.api import *
+from fabric.api import local, put, run, env
 from os import path
 
 env.hosts = ['35.231.113.169', '34.73.247.107']
@@ -18,22 +18,23 @@ def do_pack():
     local('tar -cavf {} web_static'.format(name_file))
     local('mv *.tgz versions/')
 
+
 def do_deploy(archive_path):
-    if path.exists('archive_path') == False:
+    if not path.exists(archive_path):
         return False
-    aux = [x for x in archive_path.split('.') if x.strip()]
-    tmp = aux[0]
+    tmp = archive_path[9:]
+    tmp1 = archive_path[9:-4]
     try:
-        put('archive_path', "/tmp/")
-        run('mkdir -p /data/web_static/releases/{}'.format(tmp))
-        run('tar -xzf /tmp/{} -C \
-        /data/web_static/releases/{}'.format(archive_path, tmp))
-        run('rm /tmp/{}'.format(archive_path))
+        put(archive_path, '/tmp/')
+        run('mkdir -p /data/web_static/releases/{}'.format(tmp1))
+        run('tar -xzf /tmp/{} -C\
+        /data/web_static/releases/{}/'.format(tmp, tmp1))
+        run('rm /tmp/{}'.format(tmp))
         run('mv /data/web_static/releases/{}/web_static/* \
-        /data/web_static/releases/{}/'.format(tmp, tmp))
-        run("rm -rf /data/web_static/releases/{}/web_static".format(tmp))
+        /data/web_static/releases/{}/'.format(tmp1, tmp1))
+        run("rm -rf /data/web_static/releases/{}/web_static".format(tmp1))
         run('rm -rf /data/web_static/current')
-        run('ln -s /data/web_static/releases/{}/ /data/web_static/current'.format(tmp))
-        return True
-    except:
+        run('ln -s /data/web_static/releases/{}/\
+         /data/web_static/current'.format(tmp1))
+    except Exception:
         return False
