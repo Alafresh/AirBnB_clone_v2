f6b66e9f68010bb8b860a48e722366d4c8f30bfa
Alafresh <868@holbertonschool.com>
2020-01-22 21:57:47 -0500

    last changes

---
+++ web_flask/9-states.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+from flask import Flask, escape, render_template
+from models import storage
+from models.state import State
+from models.city import City
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.teardown_appcontext
+def teardown_db(zzz):
+    storage.close()
+
+
+@app.route('/states')
+@app.route('/states/<id>')
+def display(id=None):
+    state = storage.all('State')
+    return render_template('9-states.html', state=state, id=id)
+
+if __name__ == "__main__":
+    app.run(debug=True)
